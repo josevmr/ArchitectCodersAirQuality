4d817785ad7a52ac03ff24b504b4e163f650689e
pruebMulti <josev.mirabel@gmail.com>
2024-10-06 19:15:50 +0200

    Architect Coders Practica 2
    Navigation update

--- app/src/main/java/com/practica/calidadaire/ui/screens/Navigation.kt
+++ app/src/main/java/com/practica/calidadaire/ui/screens/Navigation.kt
@@ -9,6 +9,17 @@ import androidx.navigation.compose.rememberNavController
 import com.practica.calidadaire.ui.screens.historic.HistoricScreen
 import com.practica.calidadaire.ui.screens.home.HomeScreen
 
+sealed class NavScreen(val screen: String) {
+    data object Home: NavScreen("home")
+    data object Detail: NavScreen("historic/{${NavArgs.Location.key}}") {
+        fun onCreateRoute(location: String) = "historic/${location}"
+    }
+}
+
+enum class NavArgs(val key: String){
+    Location("location")
+}
+
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun Navigation() {
@@ -16,15 +27,15 @@ fun Navigation() {
 
     NavHost(
         navController = navController,
-        startDestination = "home"
+        startDestination = NavScreen.Home.screen
     ) {
-        composable("home") {
+        composable(NavScreen.Home.screen) {
             HomeScreen(onClick = {location ->
-                navController.navigate("historic/$location")
+                navController.navigate(NavScreen.Detail.onCreateRoute(location))
             })
         }
-        composable("historic/{location}") { backStackEntry ->
-            val location = backStackEntry.arguments?.getString("location")
+        composable(NavScreen.Detail.screen) { backStackEntry ->
+            val location = backStackEntry.arguments?.getString(NavArgs.Location.key)
             HistoricScreen(
                 location = location,
                 onBack = { navController.popBackStack() }
